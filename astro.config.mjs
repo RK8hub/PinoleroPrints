962f3a53de52f6df923a5ad2b3fdaf6cba09cde1
RK8hub <rk8dev@gmail.com>
2026-05-15 20:45:11 -0600

    fork a astro etapa:2 (deploy con dominio)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pinoleroprints.rk8.dev",
+  // base ya NO va, porque tienes dominio propio
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
